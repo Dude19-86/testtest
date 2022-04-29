a459f6e5107c285c912857871116336a97ec6999
Dude19-86 <88909475+Dude19-86@users.noreply.github.com>
2022-04-29 22:01:12 +0300

    add via upload

--- script.js
+++ script.js
@@ -347,8 +347,299 @@
 //
 // console.log(calcal(4,6))
 
-function convert(amount) {
-    console.log(28 * amount)
-}
-convert(7)
+// function convert(amount) {
+//     console.log(28 * amount)
+// }
+// convert(7)
 
+
+// Место для первой задачи
+// function sayHello(name) {
+//     return 'привет,' + name;
+// }
+//
+// console.log(sayHello('ffff'))
+//
+// // Место для второй задачи
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+// }
+//
+// console.log(returnNeighboringNumbers(5))
+
+// Место для третьей задачи
+// function getMathResult(num, time) {
+//     if (typeof(time) !== 'number' || time <= 0) {
+//         return num;
+//     }
+//     let str = '';
+//
+//     for (let i = 1; i <= time; i++) {
+//         if (i === time) {
+//             str += `${num * i}`;
+//         } else {
+//             str += `${num * i}---`;
+//         }
+//     }
+//     return str;
+// }
+//
+// console.log(getMathResult(5, 3));
+// let r = 0;
+// for (let i = 0; i < 3; i++) {
+//      r += i
+//
+// }
+//
+// console.log(r);
+
+// const str = 'ggfgj';
+//
+// console.log(str.length)
+
+
+// Место для первой задачи
+// function calculateVolumeAndArea(num) {
+//     if (!Number.isInteger(num) || num === null || num < 0 || num === undefined) {
+//         return 'При вычислении произошла ошибка';
+//     }
+//     return `Объем куба: ${Math.pow(num, 3) + }, площадь всей поверхности: ${Math.pow(num, 2) * 6}`;
+// }
+//
+// console.log(calculateVolumeAndArea(5))
+// console.log(calculateVolumeAndArea(15))
+// console.log(calculateVolumeAndArea(0))
+// console.log(calculateVolumeAndArea(15.5))
+// console.log(calculateVolumeAndArea(-15))
+// console.log(calculateVolumeAndArea(37))
+//
+// // Место для второй задачи
+// function getCoupeNumber(seatNumber) {
+//     if (typeof(seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+//         return "Ошибка. Проверьте правильность введенного номера места";
+//     }
+//
+//     if (seatNumber === 0 || seatNumber > 36) {
+//         return "Таких мест в вагоне не существует";
+//     }
+//
+//     for (let i = 4; i <= 36; i = i + 4) {
+//         if (seatNumber <= i) {
+//             return Math.ceil(i / 4);
+//         }
+//     }
+//     // тут очень много вариантов решения, но выбрал один из элегантных :)
+// }
+//
+// console.log(getCoupeNumber(4));
+
+
+// Место для первой задачи
+// function getTimeFromMinutes(min) {
+//     let hourStr = 'час';
+//     let hour = Math.floor(min / 60)
+//     let resMin = min - hour * 60;
+//     if (hour === 2 ||hour === 3 ||hour === 4) {
+//         hourStr += 'а';
+//     } else if (hour === 1) {
+//         hourStr += '';
+//     } else  {
+//         hourStr += 'ов'
+//     }
+//     if (!Number.isInteger(min) || min < 0 || typeof (min) !== 'number') {
+//         return 'Ошибка, проверьте данные';
+//     }
+//     return `Это ${hour} ${hourStr} и ${resMin} минут`;
+// }
+
+// Место для второй задачи
+// function findMaxNumber(num1, num2, num3, num4) {
+//     if (typeof (num1) !== 'number' || typeof (num2) !== 'number' || typeof (num3) !== 'number' || typeof (num4) !== 'number') {
+//         return 0;
+//     }
+//     let res = [];
+//     res.push(num1, num2, num3, num4);
+//     return res.sort((a, b) => b - a)[0];
+// }
+//
+// console.log(findMaxNumber('jj', 7, 9, 6))
+// console.log(findMaxNumber(3, 7, 9, 6))
+// console.log(findMaxNumber(3, 7.7, 9, 6))
+
+// console.log(getTimeFromMinutes(150));
+// console.log(getTimeFromMinutes(500));
+// console.log(getTimeFromMinutes(0));
+// console.log(getTimeFromMinutes(120));
+// console.log(getTimeFromMinutes(200));
+
+
+// function fib(n) {
+//     if (typeof (n) !== 'number' || n <= 0 || !Number.isInteger(n)) {
+//         return '';
+//     }
+//     let res = [];
+//     let one = 0;
+//     let two = 1;
+//     for (let i = 0; i < n; i++) {
+//         if (i + 1 === n) {
+//             res.push(one);
+//             // Без пробела в конце
+//         } else {
+//             res.push(one);
+//         }
+//         let three = one + two;
+//         one = two;
+//         two = three;
+//     }
+//     return res.join((' '));
+// }
+//
+// console.log(fib(4));
+//
+//
+// function fib2(num) {
+//     if (typeof (num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+//         return "";
+//     }
+//
+//     let result = '';
+//     let first = 0;
+//     let second = 1;
+//
+//     for (let i = 0; i < num; i++) {
+//         if (i + 1 === num) {
+//             result += `${first}`;
+//             // Без пробела в конце
+//         } else {
+//             result += `${first} `;
+//         }
+//
+//         let third = first + second;
+//         first = second;
+//         second = third;
+//     }
+//
+//     return result;
+// }
+//
+// console.log(fib2(5));
+
+
+// function first() {
+//     setTimeout(function () {
+//         console.log(1)
+//     }, 500);
+// }
+//
+// function second() {
+//     console.log(2);
+// }
+//
+// first();
+// second();
+//
+// function learnJs(lang, callback) {
+//     console.log(`я учу: ${lang}`);
+//     callback();
+// }
+//
+// function done () {
+//     console.log('я прошел этот урок!')
+// }
+//
+// learnJs('JS', done);
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     color: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function () {
+//         let ttt = 'huj';
+//         console.log(('test').toUpperCase() + ' ' + ttt.toUpperCase());
+//     }
+// };
+//
+// options.makeTest();
+// const {border, bg} = options.color;
+// const {name, height, width} = options;
+// console.log(border);
+// console.log(bg);
+// console.log(name);
+// console.log(height);
+// console.log(width);
+
+
+// console.log(options.name);
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+//
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`property ${i}, have value ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`property ${key}, have value ${options[key]}`);
+//         counter++;
+//     }
+// }
+//
+// console.log(counter);
+
+// console.log(Object.keys(options).length);
+
+
+// const arr = [1, 3, 4, 6, 7, 8, 9];
+//
+//
+// arr.forEach((item, i, arr) => {
+//     console.log(`${i}: ${item} inside array ${arr}`);
+// })
+
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort((a, b) => a - b);
+// console.log(products.join(';'));
+
+let a = 5;
+let b = a;
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+
+
+function copy (mainObj) {
+    let objCopy = {};
+    for (let key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+
+}
+
+const numbers ={
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+let newNumber = copy(numbers);
+
+newNumber.a = 10;
+
+console.log(newNumber);
+console.log(numbers);
